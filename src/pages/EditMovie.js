98a81de7407e498ffe9e64aeffcd4a6672bb4381
larissapalombo <64561249+larissapalombo@users.noreply.github.com>
2020-12-08 15:49:06 -0300

    Crud-React

---
+++ src/pages/EditMovie.js
@@ -0,0 +1,52 @@
+// projeto do Sidnei
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import { MovieForm } from '../components';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
+
+class EditMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: undefined,
+      status: 'loading',
+      shouldRedirect: false,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // https://learn.co/lessons/react-router-params
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    movieAPI.getMovie(id).then((data) => this.setState({ movie: data, status: '' }));
+  }
+
+  handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie).then(() => this.setState({ shouldRedirect: true }));
+  }
+
+  render() {
+    const { status, shouldRedirect, movie } = this.state;
+    if (shouldRedirect) {
+      return <Redirect to="/" />;
+    }
+
+    if (status === 'loading') {
+      return <Loading />;
+    }
+
+    return (
+      <div data-testid="edit-movie">
+        <MovieForm movie={movie} onSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
+}
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({ params: PropTypes.shape({ id: PropTypes.string }) }).isRequired,
+};
+
+export default EditMovie;
